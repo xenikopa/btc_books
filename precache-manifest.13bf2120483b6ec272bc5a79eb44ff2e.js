78c7097e7636f1645ce9c35eb693d27aeefadc8c
Xeni Kopa <xenikopa@MacBook-Pro-Xeni.local>
2020-04-03 15:59:23 +0300

    Updates

---
+++ precache-manifest.13bf2120483b6ec272bc5a79eb44ff2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9f45e1e3c1016568c24",
+    "url": "/static/css/main.4dadbb43.chunk.css"
+  },
+  {
+    "revision": "c9f45e1e3c1016568c24",
+    "url": "/static/js/main.4a618d58.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ea1f5d6ca22bdf6a6628",
+    "url": "/static/js/2.75732e54.chunk.js"
+  },
+  {
+    "revision": "e10635ff3da0341144bdfddbbfe067be",
+    "url": "/static/media/books.e10635ff.jpg"
+  },
+  {
+    "revision": "c0b76443a2bd7bf6e4a02bd855d85905",
+    "url": "/static/media/MyriadPro-Light.c0b76443.woff"
+  },
+  {
+    "revision": "7cce27d974e442554b248f0d2832908e",
+    "url": "/static/media/MyriadPro-Light.7cce27d9.eot"
+  },
+  {
+    "revision": "f4bf8ccd575532aae5e5b07b1749de32",
+    "url": "/static/media/MyriadPro-Light.f4bf8ccd.ttf"
+  },
+  {
+    "revision": "54b17fd6776c706596d5486f53a229bb",
+    "url": "/index.html"
+  }
+];
