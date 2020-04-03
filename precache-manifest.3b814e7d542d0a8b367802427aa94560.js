39cc8a3d2efec68a9a063303d1a88bda9469853d
xenikopa <xenikopa@users.noreply.github.com>
2020-04-03 13:20:30 +0000

    Updates

---
+++ precache-manifest.3b814e7d542d0a8b367802427aa94560.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "640b2058705c818897bc",
+    "url": "/react-redux-books/static/css/main.5261f13d.chunk.css"
+  },
+  {
+    "revision": "640b2058705c818897bc",
+    "url": "/react-redux-books/static/js/main.3e6ebc8a.chunk.js"
+  },
+  {
+    "revision": "a5a92572dbd2c294a443",
+    "url": "/react-redux-books/static/js/runtime~main.352029ac.js"
+  },
+  {
+    "revision": "78dc6c881771eb832e48",
+    "url": "/react-redux-books/static/js/2.2ad05943.chunk.js"
+  },
+  {
+    "revision": "e10635ff3da0341144bdfddbbfe067be",
+    "url": "/react-redux-books/static/media/books.e10635ff.jpg"
+  },
+  {
+    "revision": "7cce27d974e442554b248f0d2832908e",
+    "url": "/react-redux-books/static/media/MyriadPro-Light.7cce27d9.eot"
+  },
+  {
+    "revision": "c0b76443a2bd7bf6e4a02bd855d85905",
+    "url": "/react-redux-books/static/media/MyriadPro-Light.c0b76443.woff"
+  },
+  {
+    "revision": "f4bf8ccd575532aae5e5b07b1749de32",
+    "url": "/react-redux-books/static/media/MyriadPro-Light.f4bf8ccd.ttf"
+  },
+  {
+    "revision": "3dae88e3433e5984f38e4831b8da2810",
+    "url": "/react-redux-books/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ecc9211c4ce38ab5f557a21d564b782.js"
+  "/react-redux-books/precache-manifest.3b814e7d542d0a8b367802427aa94560.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-redux-books/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
